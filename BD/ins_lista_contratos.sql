2610705aaeef9f3ccc1a6654948ac26e636503f7
Sebas320 <cbastian320@gmail.com>
2020-02-16 21:46:52 -0500

    Update ins_lista_contratos.sql

--- BD/ins_lista_contratos.sql
+++ BD/ins_lista_contratos.sql
@@ -1,6 +1,6 @@
 insert into lista_contratos values(1, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
-insert into lista_contratos values(3, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
-insert into lista_contratos values(4, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
-insert into lista_contratos values(5, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
+--insert into lista_contratos values(3, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
+--insert into lista_contratos values(4, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
+--insert into lista_contratos values(5, new contratosobj(400, 101, TO_DATE('07/01/2018','dd/MM/yyyy'), TO_DATE('07/01/2018','dd/MM/yyyy'), 828116, 2));
 
 
